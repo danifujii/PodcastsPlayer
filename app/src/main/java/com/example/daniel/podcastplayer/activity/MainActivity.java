08a9adf73d9bbced6471e39045236acf2b738e89
Daniel <danifujii@gmail.com>
2016-09-27 10:16:53 -0300

    Cambiado a BottomBar a TabLayout No se deja suscribir hasta que la descarga de información haya terminado, evitando una inconsistencia donde se dejaba suscribir pero los episodios no se guardaban en la BD.

--- app/src/main/java/com/example/daniel/podcastplayer/activity/MainActivity.java
+++ app/src/main/java/com/example/daniel/podcastplayer/activity/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.daniel.podcastplayer.activity;
 
 import android.support.annotation.IdRes;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.TableLayout;
 
 import com.example.daniel.podcastplayer.NewPodcastsFragment;
 import com.example.daniel.podcastplayer.R;
@@ -14,12 +21,10 @@ import com.example.daniel.podcastplayer.SubscriptionsFragment;
 import com.example.daniel.podcastplayer.data.DbHelper;
 import com.example.daniel.podcastplayer.player.PlayerSheetManager;
 import com.example.daniel.podcastplayer.player.PodcastPlayerService;
-import com.roughike.bottombar.BottomBar;
-import com.roughike.bottombar.OnTabSelectListener;
 
 public class MainActivity extends AppCompatActivity {
 
-    private BottomBar bottomBar;
+    private ViewPager viewPager;
     private FrameLayout fragmentContainer;
 
     @Override
@@ -27,41 +32,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (findViewById(R.id.fragment_layout) != null)
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.fragment_layout, new NewPodcastsFragment()).commit();
+        //if (findViewById(R.id.fragment_layout) != null)
+        //    getSupportFragmentManager().beginTransaction()
+        //            .add(R.id.fragment_layout, new NewPodcastsFragment()).commit();
 
-        bottomBar = (BottomBar)findViewById(R.id.bottom_bar);
-        bottomBar.setDefaultTab(R.id.tab_new);
-        if (bottomBar!=null)
-            bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
-                @Override
-                public void onTabSelected(@IdRes int tabId) {
-                    switch(tabId){
-                        case R.id.tab_new:{
-                            if (findViewById(R.id.fragment_layout) != null)
-                                getSupportFragmentManager().beginTransaction()
-                                        .replace(R.id.fragment_layout, new NewPodcastsFragment()).commit();
-                            break;
-                        }
-                        case R.id.tab_search:{
-                            if (findViewById(R.id.fragment_layout) != null)
-                                getSupportFragmentManager().beginTransaction()
-                                        .replace(R.id.fragment_layout, new SearchFragment()).commit();
-                            break;
-                        }
-                        case R.id.tab_subscriptions:{
-                            if (findViewById(R.id.fragment_layout) != null)
-                                getSupportFragmentManager().beginTransaction()
-                                        .replace(R.id.fragment_layout, new SubscriptionsFragment()).commit();
-                            break;
-                        }
-                        default:{
 
-                        }
-                    }
-                }
-            });
+        viewPager = (ViewPager)findViewById(R.id.viewPager);
+        viewPager.setAdapter(new TabPagerAdapter(getSupportFragmentManager()));
+        viewPager.setCurrentItem(1);
+        TabLayout tl = (TabLayout) findViewById(R.id.tabLayout);
+        tl.setupWithViewPager(viewPager);
+
     }
 
     @Override
@@ -73,6 +54,39 @@ public class MainActivity extends AppCompatActivity {
             findViewById(R.id.splayer_layout).setVisibility(View.VISIBLE);
             PlayerSheetManager psm = new PlayerSheetManager();
             psm.setSheetInterface(pps.getEpisode(),this);
+        }
+    }
+
+    private class TabPagerAdapter extends FragmentStatePagerAdapter {
+        private static final int numPages = 3;
+
+        public TabPagerAdapter(FragmentManager fm){
+            super(fm);
+        }
+
+        @Override
+        public int getCount() {
+            return numPages;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch(position) {
+                case 0: return new SearchFragment();
+                case 1: return new NewPodcastsFragment();
+                case 2: return new SubscriptionsFragment();
+            }
+            return null;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch(position){
+                case 0: return getString(R.string.tab_discover);
+                case 1: return getString(R.string.tab_new);
+                case 2: return getString(R.string.tab_podcasts);
+            }
+            return null;
         }
     }
 }
